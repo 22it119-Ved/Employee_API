ad214251e6df8b5593dcf53ff134029c6162fd3c
22it119-Ved <165412428+22it119-Ved@users.noreply.github.com>
2024-10-19 23:24:27 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
